1afd5bce8574401fda80813d6e0e7e54420fc7d6
Yevhenii Romanyuk <hotpick18@gmail.com>
2025-02-26 03:33:30 +0100

    init commit

---
+++ src/components/FarmGrid/farmGrid.tsx
@@ -0,0 +1,20 @@
+import classes from './farmGrid.module.css';
+
+const FarmGrid = () => {
+  return (
+    <div className={classes.wrap}>
+      <div className={classes.root}>
+        <div className={classes.container}>
+          {Array.from({length: 52}, (_, index) => (
+            <div key={index} className={classes.item}/>
+          ))}
+        </div>
+        <img src="./ui_bulding_5_1 1.png" alt="logo"
+             className="absolute bottom-[40vw] left-[50%] transform -translate-x-1/2 w-[38vw] z-20"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default FarmGrid;
